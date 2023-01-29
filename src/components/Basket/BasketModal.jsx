342bd9be99a53e7619e1dc06cb36808146c4a290
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-01-29 18:30:28 +0330

    adding payment and editing styles

--- src/components/Basket/BasketModal.jsx
+++ src/components/Basket/BasketModal.jsx
@@ -1,19 +1,36 @@
 import React from "react";
 import Nothing from "./Nothing";
 import "./BasketModal.scss";
-import MenuBuilder from "../Menu/MenuBuilder";
 import BasketMenuBuilder from "./BasketMenuBuilder";
 const BasketModal = (props) => {
   const onAdd = (a) => {
     props.onAdd(a);
   };
   let menu = <Nothing />;
+  let total = 0;
   if (props.data.length !== 0) {
-    menu = <BasketMenuBuilder onAdd={onAdd}  data={props.data} />;
+    menu = <BasketMenuBuilder onAdd={onAdd} data={props.data} />;
+    total = props.data.reduce((a, b) => {
+      return (a += b.amount * b.price);
+    }, 0);
   }
-  return <div className="modalPopup">{menu}
-  <a onClick={props.onClose} className="closeButton"><span>Close</span></a>
-  </div>;
+  return (
+    <div className="modalPopup">
+      {menu}
+      {props.data.length>0 && <div className="BItemTotal">
+        <div className="BItemTotal__text">&lArr; Total</div>
+        <div className="BItemTotal__amount">{total}</div>
+      </div>}
+      <div className="buttonsContainerBasket">
+        <a onClick={props.onClose} className="basketButton">
+          <span>Close</span>
+        </a>
+        {props.data.length>0 &&<a onClick={props.onClose} className="basketButton pay">
+          <span>Pay</span>
+        </a>}
+      </div>
+    </div>
+  );
 };
 
 export default BasketModal;
